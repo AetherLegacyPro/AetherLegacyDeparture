4e8a9edf50ec6e2bc8ca3f4616199d3043cfd6cb
Charsy89 <28523197+Charsy89@users.noreply.github.com>
2026-06-28 13:20:14 -0600

    Removed redundant duplicate logic in overridden methods

--- src/main/java/com/gildedgames/the_aether/client/renders/AetherItemRenderer.java
+++ src/main/java/com/gildedgames/the_aether/client/renders/AetherItemRenderer.java
@@ -44,8 +44,6 @@ public class AetherItemRenderer extends ItemRenderer {
 
 	private float prevEquippedProgress;
 
-	private int equippedItemSlot = -1;
-
 	public AetherItemRenderer(Minecraft mcIn)
 	{
 		super(mcIn);
@@ -385,58 +383,6 @@ public class AetherItemRenderer extends ItemRenderer {
         {
         	mods.battlegear2.client.utils.BattlegearRenderHelper.renderItemInFirstPerson(partialTicks, mc, this);
         }
-	}
-
-	@Override
-	public void updateEquippedItem() {
-		super.updateEquippedItem();
-
-		this.prevEquippedProgress = this.equippedProgress;
-		EntityClientPlayerMP entityclientplayermp = this.mc.thePlayer;
-		ItemStack itemstack = entityclientplayermp.inventory.getCurrentItem();
-		boolean flag = this.equippedItemSlot == entityclientplayermp.inventory.currentItem && itemstack == this.itemToRender;
-
-		if (this.itemToRender == null && itemstack == null) {
-			flag = true;
-		}
-
-		if (itemstack != null && this.itemToRender != null && itemstack != this.itemToRender && itemstack.getItem() == this.itemToRender.getItem() && itemstack.getItemDamage() == this.itemToRender.getItemDamage()) {
-			this.itemToRender = itemstack;
-			flag = true;
-		}
-
-		float f = 0.4F;
-		float f1 = flag ? 1.0F : 0.0F;
-		float f2 = f1 - this.equippedProgress;
-
-		if (f2 < -f) {
-			f2 = -f;
-		}
-
-		if (f2 > f) {
-			f2 = f;
-		}
-
-		this.equippedProgress += f2;
-
-		if (this.equippedProgress < 0.1F) {
-			this.itemToRender = itemstack;
-			this.equippedItemSlot = entityclientplayermp.inventory.currentItem;
-		}
-	}
-
-	@Override
-	public void resetEquippedProgress() {
-		super.resetEquippedProgress();
-
-		this.equippedProgress = 0.0F;
-	}
-
-	@Override
-	public void resetEquippedProgress2() {
-		super.resetEquippedProgress2();
-
-		this.equippedProgress = 0.0F;
 	}
 
 }
